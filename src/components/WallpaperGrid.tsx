b9c93958dd96b9fc14137806143b908355b770bb
[dyad] <easirarafat.pixel.g@gmail.com>
2025-07-22 15:52:30 +0600

    [dyad] Fixed incorrect Supabase client import. - wrote 1 file(s)

--- src/components/WallpaperGrid.tsx
+++ src/components/WallpaperGrid.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import supabase from "@/integrations/supabase/client";
+import { supabase } from "@/integrations/supabase/client";
 import WallpaperCard, { type Wallpaper } from "./WallpaperCard";
 import { Skeleton } from "@/components/ui/skeleton";
 import { showError } from "@/utils/toast";
